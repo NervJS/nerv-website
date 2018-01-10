89177441bc5f3ae49779d12095f5a4d3d109c051
yuche <i@yuche.me>
2018-01-11 01:25:12 +0800

    fix: typo

--- src/locales/en.js
+++ src/locales/en.js
@@ -2,7 +2,7 @@ export default {
   home: 'Home',
   docs: 'Docs',
   banner_desc:
-    'A blazing fast React alternative, compatiable with IE8 and React 16.',
+    'A blazing fast React alternative, compatible with IE8 and React 16.',
   getting_started: 'Getting started',
   features: 'Features',
   benchmarks: 'Benchmarks',
